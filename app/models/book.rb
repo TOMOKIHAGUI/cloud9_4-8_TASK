1d88826532ad453bdc98a2f753f97b9e390d5dd0
TOMOKIHAGUI <higosozan18@gmail.com>
2020-12-12 12:24:17 +0000

    レイアウト調整

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validates :title, presence: true
-  validates :body, presence:true
+  validates :title, presence: { message: "Error!" }
+  validates :body, presence: { message: "Error!" }
 end
